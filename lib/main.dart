edd51178b922a1bbd468a069cd821921aecae485
Yash-Garg <yash.garg@outlook.in>
2021-04-04 21:48:11 +0530

    Add basic gridview for new photos
    - Set per page photos to 20
    - Implement imageList from getJsonData
    - Use google fonts and change colorscheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
 import 'package:flutsplash/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   await DotEnv.load(fileName: ".env");
   runApp(MyApp());
 }
 
+const Color materialDark = Color(0xFF121212);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        primarySwatch: Colors.grey,
+        primaryColor: materialDark,
+        brightness: Brightness.dark,
+        backgroundColor: materialDark,
+        accentColor: const Color(0xFFCD57FF),
+        scaffoldBackgroundColor: materialDark,
+        fontFamily: GoogleFonts.workSans().fontFamily,
+      ),
       home: HomePage(),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:flutsplash/models/photo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 String accessKey = env['ACCESS_KEY']!;
-String apiURL = "https://api.unsplash.com/photos/?client_id=$accessKey";
+String apiURL =
+    "https://api.unsplash.com/photos/?client_id=$accessKey&per_page=20";
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,29 +14,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future getJSON() async {
+  Future _getJsonData() async {
     Dio dio = new Dio();
     var response = await dio.get(apiURL);
-    print(response.data[0]); // for checking output
+    List<dynamic> jsonData = response.data;
+    List<Photo> imageList = jsonData.map((d) => Photo.fromJson(d)).toList();
+    return imageList;
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: new Center(
-            child: Text("FlutSplash",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20))),
-      ),
-      body: new Center(
-        child: new OutlinedButton(
-            child: new Text("Send Request",
-                style: TextStyle(fontSize: 20, color: Colors.black)),
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.orange)),
-            onPressed: () async {
-              await getJSON();
-            }),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "FlutSplash",
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 25,
+                letterSpacing: 1,
+              ),
+            ),
+          ),
+        ),
+        body: Container(
+          child: FutureBuilder(
+            future: _getJsonData(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData)
+                return StaggeredGridView.countBuilder(
+                  shrinkWrap: true,
+                  crossAxisCount: 4,
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: MediaQuery.of(context).size.height * 0.75,
+                      width: MediaQuery.of(context).size.width * 0.50,
+                      margin: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(snapshot.data![index].urls.small),
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    );
+                  },
+                  staggeredTileBuilder: (int index) =>
+                      new StaggeredTile.count(2, index.isEven ? 3 : 1.5),
+                  mainAxisSpacing: 2.0,
+                  crossAxisSpacing: 2.0,
+                );
+              else
+                return Center(child: CircularProgressIndicator());
+            },
+          ),
+        ),
       ),
     );
   }
