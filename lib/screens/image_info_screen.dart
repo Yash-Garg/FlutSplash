c4383dc07d65541f7e80e7d55da51bdff85296ee
Yash-Garg <yash.garg@outlook.in>
2021-04-07 19:06:56 +0530

    Add user info, download function and open in gallery on image info screen

--- lib/screens/image_info_screen.dart
+++ lib/screens/image_info_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:io' as io;
+import 'package:flutsplash/helpers/download_manager.dart';
+import 'package:flutsplash/helpers/keys.dart';
+import 'package:flutsplash/helpers/path_manager.dart';
+import 'package:flutsplash/helpers/permission_manager.dart';
+import 'package:flutsplash/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
 import 'package:share/share.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -17,9 +24,20 @@ class _ImageInfoScreenState extends State<ImageInfoScreen> {
 
   @override
   Widget build(BuildContext context) {
+    String accessKey = Keys.UNSPLASH_API_CLIENT_ID;
     String webURL = widget.imageDetails['links']['html'];
-    String imgURL = widget.imageDetails['urls']['regular'];
+    String imgURL = widget.imageDetails['urls']['small'];
+    String rawImgURL = widget.imageDetails['urls']['raw'];
+    String imgID = widget.imageDetails['id'];
     String imgCreator = widget.imageDetails['user']['name'];
+    String downloadEndpoint = widget.imageDetails['links']['download_location'];
+    String creatorPic = widget.imageDetails['user']['profile_image']['medium'];
+
+    Future checkImage(String id) async {
+      String imagePath = await getPath("$id.jpeg");
+      var result = await io.File(imagePath).exists();
+      return result;
+    }
 
     return SafeArea(
       child: Scaffold(
@@ -51,6 +69,75 @@ class _ImageInfoScreenState extends State<ImageInfoScreen> {
                 ),
                 onTap: () {},
               ),
+              Wrap(
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(20, 15, 20, 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                CircleAvatar(
+                                  backgroundImage: NetworkImage("$creatorPic"),
+                                  radius: 20,
+                                ),
+                                Padding(padding: EdgeInsets.only(left: 10)),
+                                Text(
+                                  "$imgCreator",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.download_outlined,
+                                    size: 25,
+                                  ),
+                                  onPressed: () async {
+                                    var downloadPath =
+                                        await getPath("$imgID.jpeg");
+                                    await dio.get(
+                                        "$downloadEndpoint&client_id=$accessKey");
+                                    await downloadFile(
+                                        rawImgURL, downloadPath, context);
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.favorite_border,
+                                    size: 25,
+                                  ),
+                                  onPressed: () {},
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                              child: Divider(
+                                color: Colors.white38,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             ],
           ),
         ),
@@ -60,14 +147,25 @@ class _ImageInfoScreenState extends State<ImageInfoScreen> {
               Icon(Icons.wallpaper),
               Padding(
                 child: Text(
-                  "SET AS WALLPAPER",
+                  "OPEN IN GALLERY",
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
                 padding: EdgeInsets.only(left: 10),
               ),
             ],
           ),
-          onPressed: () {},
+          onPressed: () async {
+            var imgExist = await checkImage("$imgID");
+            var status = await requestPermissions();
+            String filePath = await getPath("$imgID.jpeg");
+            if (imgExist == true) {
+              print(filePath);
+              OpenFile.open("$filePath");
+            } else if (status == true) {
+              await dio.get("$downloadEndpoint&client_id=$accessKey");
+              await downloadFile(rawImgURL, filePath, context);
+            }
+          },
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       ),
