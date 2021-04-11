1a5e2cb9dc7887d16b840d6050c310d56c8aa15c
Yash-Garg <yash.garg@outlook.in>
2021-04-12 02:19:20 +0530

    Add searchBar implementation fully

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutsplash/helpers/keys.dart';
 import 'package:flutsplash/main.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -11,40 +11,131 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   String accessKey = Keys.UNSPLASH_API_CLIENT_ID;
-  TextEditingController searchController = new TextEditingController();
-  Dio dio = new Dio();
+  late TextEditingController searchController;
   var searchResults;
+  bool isSubmitted = false;
+  Dio dio = new Dio();
 
   Future _getSearchResults(String query) async {
     String searchURL =
-        "https://api.unsplash.com/search/photos?query=$query&client_id=$accessKey";
+        "https://api.unsplash.com/search/photos?query=$query&client_id=$accessKey&per_page=20";
     var response = await dio.get(searchURL);
     Map<String, dynamic> searchData = response.data;
     return searchData;
   }
 
+  @override
+  void initState() {
+    super.initState();
+    searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
   Widget _buildSearchBar() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.black12,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      margin: EdgeInsets.symmetric(horizontal: 15),
-      padding: EdgeInsets.symmetric(horizontal: 15),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: TextField(
-              autofocus: true,
-              controller: searchController,
-              decoration: InputDecoration(
-                hintText: "Search images...",
-                border: InputBorder.none,
-              ),
-            ),
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.black12,
+            borderRadius: BorderRadius.circular(10),
           ),
-          InkWell(onTap: () {}, child: Container(child: Icon(Icons.search)))
-        ],
+          margin: EdgeInsets.symmetric(horizontal: 15),
+          padding: EdgeInsets.only(left: 15),
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: Icon(Icons.search),
+              ),
+              Expanded(
+                child: TextField(
+                  cursorColor: Colors.black,
+                  cursorHeight: 20,
+                  autofocus: true,
+                  enableSuggestions: true,
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    hintText: "Search images...",
+                    border: InputBorder.none,
+                  ),
+                  onSubmitted: (value) async {
+                    searchResults = await _getSearchResults(value);
+                    setState(() {
+                      isSubmitted = true;
+                    });
+                  },
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  setState(() {
+                    isSubmitted = false;
+                    searchController.clear();
+                  });
+                  ;
+                },
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: isSubmitted
+              ? _buildSearchResultList()
+              : Container(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "It's too empty here..",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSearchResultList() {
+    var data = searchResults["results"];
+    return Container(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: StaggeredGridView.countBuilder(
+          physics: ScrollPhysics(),
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: MediaQuery.of(context).size.height * 0.75,
+              width: MediaQuery.of(context).size.width * 0.50,
+              margin: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage("${data[index]['urls']['small']}"),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(5),
+              ),
+            );
+          },
+          staggeredTileBuilder: (int index) =>
+              new StaggeredTile.count(2, index.isEven ? 3 : 1.5),
+          shrinkWrap: true,
+          itemCount: data.length,
+          mainAxisSpacing: 2.0,
+          crossAxisSpacing: 2.0,
+          crossAxisCount: 4,
+        ),
       ),
     );
   }
@@ -53,30 +144,16 @@ class _SearchScreenState extends State<SearchScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: accentClr,
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            "Search Page",
-            style: TextStyle(
-              fontWeight: FontWeight.w900,
-              fontSize: 20,
-              color: Colors.black,
-              fontFamily: GoogleFonts.paytoneOne().fontFamily,
-              letterSpacing: 1,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                SizedBox(height: 16),
+                _buildSearchBar(),
+                SizedBox(height: 30),
+              ],
             ),
-          ),
-        ),
-        iconTheme: IconThemeData(color: Colors.black),
-        actions: [IconButton(icon: Icon(Icons.more_vert), onPressed: () {})],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-              SizedBox(height: 16),
-              _buildSearchBar(),
-              SizedBox(height: 30),
-            ],
           ),
         ),
       ),
